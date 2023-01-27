0a58fe1f0036136e5efe5cfcd3b34ba1e537f57d
teikun-86 <--get-all>
2023-01-27 14:34:05 +0700

    set midtrans env to dynamic

--- src/libs/midtrans.js
+++ src/libs/midtrans.js
@@ -1,7 +1,7 @@
 import Midtrans from "midtrans-client";
 
 export const midtransClient = new Midtrans.Snap({
-    isProduction: false,
+    isProduction: process.env.MIDTRANS_ENV === 'production',
     serverKey: process.env.MIDTRANS_SERVER_KEY,
     clientKey: process.env.NEXT_PUBLIC_MIDTRANS_CLIENT_KEY,
 })
